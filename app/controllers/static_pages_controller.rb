3d50fc672df66da95fb540d3f38afdc617555425
Tam <nguyenthinhungtam@gmail.com>
2018-11-15 11:07:53 +0700

    name

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,11 +5,10 @@ class StaticPagesController < ApplicationController
       @feed_items = current_user.feed.paginate page: params[:page]
     end
   end
-
   def help
   end
   def hello
   	puts "hello world"
-  	
+  	puts "what your name?"
   end
 end
